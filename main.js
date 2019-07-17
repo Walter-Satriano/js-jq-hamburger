49c3dc778bcf76744c947ab9c64e70fc959a90a6
Walter Satriano <walter.satri@gmail.com>
2019-07-17 10:20:17 +0200

    aggiunto metodo per mantenere la responsivness

--- main.js
+++ main.js
@@ -4,14 +4,29 @@ Creare un hamburger menu con l’utilizzo di Html, Css e JavaScript.*/
 
 $(document).ready(function(){
 
-  // cliccando sull'icona hamburger si apre il menù
+  // // cliccando sull'icona hamburger si apre il menù
+  // $(".header-right > a").click(function() {
+  //   $(".hamburger-menu").show();
+  //   // $(".hamburger-menu").addClass("active");
+  // });
+  //
+  // // cliccando sulla X si chiude il menù
+  // $(".close").click(function() {
+  //   $(".hamburger-menu").hide();
+  //   // $(".hamburger-menu").removeClass("active");
+  // });
+
+
+  // METODO ALTERNATIVO PER RESPONSIVE
+  // cliccando sull'icona hamburger si apre il menù e quando allargo il browser
+  // oltre un certo viewport il menù hamburger scompare automaticamente
   $(".header-right > a").click(function() {
-    $(".hamburger-menu").show();
+    $(".hamburger-menu").addClass("active");
   });
 
   // cliccando sulla X si chiude il menù
   $(".close").click(function() {
-    $(".hamburger-menu").hide();
+    $(".hamburger-menu").removeClass("active");
   });
 
 });
